301cba3c8f0c2f5786f7880a7319907a87386e67
PABLO ALVES OLIVEIRA <pabloalves1213@gmail.com>
2025-10-21 23:27:24 -0300

    #WIP-Criando a casca da task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomLayoutComponent } from './custom-layout/custom-layout.component';
+import { ClienteComponent } from './custom-layout/cliente/cliente.component';
+import { MedicoComponent } from './custom-layout/medico/medico.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CustomLayoutComponent,
+    children: [
+      { path: '', component: ClienteComponent },
+      { path: 'cliente', component: ClienteComponent },
+      { path: 'medico', component: MedicoComponent },
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // preloadingStrategy: PreloadAllModules,
+    scrollPositionRestoration: 'enabled',
+    relativeLinkResolution: 'corrected',
+    anchorScrolling: 'enabled'
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VexModule } from '../@vex/vex.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CustomLayoutModule } from './custom-layout/custom-layout.module';
+import { ClienteComponent } from './custom-layout/cliente/cliente.component';
+import { MedicoComponent } from './custom-layout/medico/medico.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent,ClienteComponent,MedicoComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    // Vex
+    VexModule,
+    CustomLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
