4c57e5a4fca94dce394ca7b602769fdcb0849db1
PABLO ALVES OLIVEIRA <pabloalves1213@gmail.com>
2025-10-27 21:01:14 -0300

    #WIP - iniciando busca do Api do viacep

--- src/app/model/cliente.service.ts
+++ src/app/model/cliente.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  urlApi = environment.url_api;
+
+  constructor(private http: HttpClient) { }
+
+  buscarcep(cep: string): Observable <any> {
+    return this.http.get(this.urlApi + '');
+   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  url_api: 'http://viacep.com.br/ws'
+}; 
+
 
 /*
  * For easier debugging in development mode, you can import the following file
